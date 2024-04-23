d7bc9f962fbcb4a2de4d782f3fddc0b867fc1ab2
Luís Diogo <luis.manuel.diogo@gmail.com>
2024-04-24 00:13:25 +0100

    pronto para mocks

--- PCReplay/src/App.jsx
+++ PCReplay/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import ProfilePage from "./pages/ProfilePage";
 // Importe outros componentes de página conforme necessário
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/profilePage" element={<ProfilePage />} />
+       
       </Routes>
     </Router>
   );
