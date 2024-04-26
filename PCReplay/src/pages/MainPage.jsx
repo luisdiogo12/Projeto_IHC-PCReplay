f619bd33d8294dea3939fef9caf6e2ecb0ed3e31
Luís Diogo <luis.manuel.diogo@gmail.com>
2024-04-26 03:19:55 +0100

    criacao LayoutProductPage
    vai servir de layout para outras paginas semelhantes para n estar sempre a repetir

--- PCReplay/src/pages/MainPage.jsx
+++ PCReplay/src/pages/MainPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Header from "../components/Header";
+import LayoutProductPage from "./LayoutProductPage";
 import ProductCard from "../components/ProductCard";
-import Footer from "../components/Footer";
 import { useProducts } from "../mocks/ProductContext";
 import { fetchProducts } from "../mocks/api";
 
@@ -20,20 +19,13 @@ const MainPage = () => {
       });
   }, []); // Dependência no array de useEffect, removida para evitar loop infinito
 
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <div className="flex-grow container mx-auto px-4 sm:px-8 pt-16">
-        <div className="py-8">
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </div>
+   return (
+    <LayoutProductPage>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+      {error && <p>Error: {error}</p>}
+    </LayoutProductPage>
   );
 };
 
