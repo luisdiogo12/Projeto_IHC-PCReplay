778448542c38fb1f4c95f650c10a704c510bef38
Luís Diogo <luis.manuel.diogo@gmail.com>
2024-05-07 17:43:31 +0100

    t

--- PCReplay/src/components/HsearchBar.jsx
+++ PCReplay/src/components/HsearchBar.jsx
@@ -6,7 +6,7 @@ const HSearchBar = () => {
     <div className="flex bg-white rounded overflow-hidden">
       <input
         type="search"
-        placeholder="Search products..."
+        placeholder="Search products...."
         className="px-4 py-2 w-80"
       />
       <button className="bg-blue-600 px-4 text-white">
